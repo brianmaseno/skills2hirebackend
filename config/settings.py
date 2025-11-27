02b230122373edc7750df44694a5652733c65b50
brianmaseno <brianmayoga@gmail.com>
2025-11-27 21:27:55 +0300

    Allow all hosts in ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -16,7 +16,8 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-change-this-in-produc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=Csv())
+# Allow all hosts (use specific hosts in production if needed)
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 INSTALLED_APPS = [

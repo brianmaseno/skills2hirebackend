7b9608f37ecaba5f6825f0f55529d3bce03ef565
brianmaseno <brianmayoga@gmail.com>
2025-11-27 21:19:44 +0300

    Remove unused pymysql import from wsgi.py

--- config/wsgi.py
+++ config/wsgi.py
@@ -4,10 +4,6 @@ WSGI config for SkillMatchHub project.
 
 import os
 
-# Use PyMySQL as MySQLdb
-import pymysql
-pymysql.install_as_MySQLdb()
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
